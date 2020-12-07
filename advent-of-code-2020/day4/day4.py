cc921e0742cff2e6ce0625b081cb584cf20f3f81
Joana Bencatel <bencateljoana@gmail.com>
2020-12-07 17:28:50 +0000

    Solved the 4th day (part 2) challenge of the Advent of Code 2020

--- advent-of-code-2020/day4/day4.py
+++ advent-of-code-2020/day4/day4.py
@@ -43,6 +43,7 @@ According to the above rules, your improved system would report 2 valid passport
 
 Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?
 """
+import re
 
 
 def get_data_from_line(text):
@@ -53,8 +54,7 @@ def prepare_passport_info(passportFields):
     passport = {}
     for field in passportFields:
         keyValuePair = field.split(':')
-        value = keyValuePair[1]
-        passport[keyValuePair[0]] = int(value) if value.isnumeric() else value
+        passport[keyValuePair[0]] = keyValuePair[1]
     return passport
 
 
@@ -87,11 +87,13 @@ requiredFields = {
     "hcl",
     "ecl",
     "pid",
-    #"cid"
+    # "cid"
 }
+
 
 def passport_is_valid(passport):
     return passport.keys() >= requiredFields
+
 
 def get_nr_valid_passports(passports):
     validPassports = 0
@@ -101,4 +103,139 @@ def get_nr_valid_passports(passports):
     return validPassports
 
 
-print(get_nr_valid_passports(passportsToCheck))
+# print(get_nr_valid_passports(passportsToCheck))
+
+
+"""
+--- Part Two ---
+The line is moving more quickly now, but you overhear airport security talking about how passports with invalid data are getting through. Better add some data validation, quick!
+
+You can continue to ignore the cid field, but each other field has strict rules about what values are valid for automatic validation:
+
+byr (Birth Year) - four digits; at least 1920 and at most 2002.
+iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+hgt (Height) - a number followed by either cm or in:
+If cm, the number must be at least 150 and at most 193.
+If in, the number must be at least 59 and at most 76.
+hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+pid (Passport ID) - a nine-digit number, including leading zeroes.
+cid (Country ID) - ignored, missing or not.
+Your job is to count the passports where all required fields are both present and valid according to the above rules. Here are some example values:
+
+byr valid:   2002
+byr invalid: 2003
+
+hgt valid:   60in
+hgt valid:   190cm
+hgt invalid: 190in
+hgt invalid: 190
+
+hcl valid:   #123abc
+hcl invalid: #123abz
+hcl invalid: 123abc
+
+ecl valid:   brn
+ecl invalid: wat
+
+pid valid:   000000001
+pid invalid: 0123456789
+Here are some invalid passports:
+
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+Here are some valid passports:
+
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+Count the number of valid passports - those that have all required fields and valid values. Continue to treat cid as optional. In your batch file, how many passports are valid?
+"""
+
+
+def has_x_digits(value, nrDigits):
+    return value.isnumeric() and len(value) == nrDigits
+
+
+def has_valid_year(passport, key, minYear, maxYear):
+    if key in passport:
+        year = passport[key]
+        return has_x_digits(year, 4) and int(year) >= minYear and int(year) <= maxYear
+    return False
+
+
+def has_valid_height_number(height, unit, min, max):
+    if height.endswith(unit):
+        value = height.replace(unit, '')
+        if value.isnumeric():
+            value = int(value)
+            return value >= min and value <= max
+    return False
+
+
+def has_valid_height(passport):
+    if 'hgt' in passport:
+        height = passport['hgt']
+        return has_valid_height_number(height, 'cm', 150, 193) or has_valid_height_number(height, 'in', 59, 76)
+    return False
+
+
+def only_has_allowed_characters(code, search=re.compile(r'[^a-f0-9.]').search):
+    return not bool(search(code))
+
+
+def has_valid_hair_color(passport):
+    if 'hcl' in passport:
+        hairColor = passport['hcl']
+        if hairColor.startswith("#"):
+            hairColorCode = hairColor.replace("#", "")
+            return len(hairColorCode) == 6 and only_has_allowed_characters(hairColorCode)
+    return False
+
+
+def has_valid_eye_color(passport):
+    validEyeColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+    if 'ecl' in passport:
+        eyeColor = passport['ecl']
+        for option in validEyeColors:
+            if eyeColor == option:
+                return True
+    return False
+
+def has_valid_passport_id(passport):
+    if 'pid' in passport:
+        passportID = passport['pid']
+        return has_x_digits(passportID, 9)
+    return False
+
+def get_nr_completely_valid_passports(passports):
+    validPassports = 0
+    for passport in passports:
+        isValidPassport = True
+        if has_valid_year(passport, 'byr', 1920, 2002) and has_valid_year(passport, 'iyr', 2010, 2020) and has_valid_year(passport, 'eyr', 2020, 2030) and has_valid_height(passport) and has_valid_hair_color(passport) and has_valid_eye_color(passport) and has_valid_passport_id(passport):
+            validPassports += 1
+    return validPassports
+
+
+print(get_nr_completely_valid_passports(passportsToCheck))
