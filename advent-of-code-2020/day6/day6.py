39aa9b227d780ca2e98f7fd155fec0261dc4a664
Joana Bencatel <bencateljoana@gmail.com>
2020-12-08 19:13:43 +0000

    Solved the 6th day (part 2) challenge of the Advent of Code 2020

--- advent-of-code-2020/day6/day6.py
+++ advent-of-code-2020/day6/day6.py
@@ -72,3 +72,69 @@ def get_nr_questions_answered_by_all_groups():
 
 
 print(get_nr_questions_answered_by_all_groups())
+
+
+""" 
+--- Part Two ---
+As you finish the last group's customs declaration, you notice that you misread one word in the instructions:
+
+You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to which everyone answered "yes"!
+
+Using the same example as above:
+
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+This list represents answers from five groups:
+
+In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
+In the second group, there is no question to which everyone answered "yes".
+In the third group, everyone answered yes to only 1 question, a. Since some people did not answer "yes" to b or c, they don't count.
+In the fourth group, everyone answered yes to only 1 question, a.
+In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+
+For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
+
+"""
+def get_nr_common_questions_in_group(groupAnswers):
+    commonQuestions = groupAnswers[0]
+    if len(groupAnswers) > 1:
+        for personAnswers in groupAnswers:
+            commonQuestions = list(set(commonQuestions).intersection(personAnswers))
+    return len(commonQuestions)
+
+
+def get_common_questions_for_all_groups():
+    nrCommonQuestionsAnswered = 0
+    groupAnswers = []
+
+    dataFile = open("data.txt", "r")
+    for line in dataFile:
+        answer = list_questions_answered_by_person(line)
+        if len(answer) > 0:
+            groupAnswers.append(answer)
+        else:
+            nrCommonQuestionsAnswered += get_nr_common_questions_in_group(
+                groupAnswers)
+            groupAnswers = []
+    if len(groupAnswers) > 0:
+        nrCommonQuestionsAnswered += get_nr_common_questions_in_group(
+            groupAnswers)
+
+    dataFile.close()
+    return nrCommonQuestionsAnswered
+
+print(get_common_questions_for_all_groups())
